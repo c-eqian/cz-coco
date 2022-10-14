f2f4d7d28fbe8b0b8f21a957bde3d359b80e9544
C99127 <2429120006@qq.com>
2022-10-15 00:20:00 +0800

    feat: 修改部署文件6

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,14 @@
 export const siteData = JSON.parse("{\"base\":\"/cz-coco/\",\"lang\":\"zh-CN\",\"title\":\"cz-coco\",\"description\":\"组件库&工具库\",\"head\":[],\"locales\":{\"/\":{\"lang\":\"zh-CN\"},\"/us/\":{\"lang\":\"en-US\"}}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateSiteData) {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ siteData }) => {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  })
+}
