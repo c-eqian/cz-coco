e93423a427812251a1915190d4ddac645596aeec
wenhaijing <wenhaijing5990@zj.tech>
2022-10-17 16:08:20 +0800

    feat: 解决冲突

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -7,26 +7,17 @@
  * @LastEditTime: 2022-10-17 12:43:15
  */
 
-<<<<<<< HEAD
-import _button from './button';
-import _image from './image';
-=======
->>>>>>> feature/components
 import type { App } from 'vue';
 import _button from './button';
 import _icon from './icon';
+import _image from './image';
 
 export * from './button';
 export * from './message';
-<<<<<<< HEAD
 export * from './image';
-
-const components = [_button, _image];
-=======
 export * from './icon';
 
-const components = [_button, _icon];
->>>>>>> feature/components
+const components = [_button, _icon, _image];
 export default {
   install(app: App) {
     components.forEach((item) => {
